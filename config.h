ce403fd3d5bf166272d84695a4895992d0817882
AidanUnhappy <aidanczhai@gmail.com>
2022-04-10 19:36:16 +0800

    -

--- config.h
+++ config.h
@@ -211,7 +211,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ XK_ANY_MOD,           XK_F6,          swapcolors,     {.i =  0} },
+	{ MODKEY,               XK_r,           swapcolors,     {.i =  0} },
 };
 
 /*
