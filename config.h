1418bae8fb718d0c92d92381b5f4681f0b9189d4
AidanUnhappy <aidanczhai@gmail.com>
2022-04-10 19:46:22 +0800

    patch scrollback

--- config.h
+++ config.h
@@ -212,6 +212,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_r,           swapcolors,     {.i =  0} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
